47bbb34898dfd85d3a4bb21809857e5e76f43735
krussell2 <krussell2@academic.rrc.ca>
2025-03-19 00:27:42 -0500

    Adding filtering by Subject functionality to the Books page

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ApplicationRecord
   validates :title, :work_key, :description, :subject, :first_publish_year, :edition_count, :cover_identifier, presence: true
 
   def self.ransackable_attributes(auth_object = nil)
-    [ "title" ]
+    [ "title", "subject" ]
   end
 end
