43b3e428ae485d3f239dbeb2285ddf4598bb26ef
krussell2-rrc <143433171+krussell2-rrc@users.noreply.github.com>
2025-09-16 15:04:42 -0500

    Disabled points in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.solid_queue.enabled = false
+  config.solid_cable.enabled = false
+
 end
